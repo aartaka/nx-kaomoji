952271cee42d9eebdbe744f294b6ba3bbbc3c7a8
Artyom Bologov <aartaka@protonmail.com>
2023-05-02 16:38:06 +0400

    package: 3-pre-release-7 mode package renamings.

--- package.lisp
+++ package.lisp
@@ -21,10 +21,15 @@
 
 #+nyxt-3
 (nyxt:define-package #:nx-kaomoji
-    (:import-from #:nyxt/autofill-mode
-                  #:autofill
-                  #:autofill-name
-                  #:autofill-fill)
+  (:import-from
+   #+(or 3-pre-release-1 3-pre-release-2 3-pre-release-3 3-pre-release-4 3-pre-release-5 3-pre-release-6)
+   #:nyxt/autofill-mode
+   #-(or 3-pre-release-1 3-pre-release-2 3-pre-release-3 3-pre-release-4 3-pre-release-5 3-pre-release-6)
+   #:nyxt/mode/autofill
+   #:autofill
+   #:autofill-name
+   #:autofill-fill
+   #:autofill-source)
   (:export #:kaomoji-fill
            #:*kaomojis*
            #:parse-kaomojis))
