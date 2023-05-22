a9c6364e137b32fb6330f08a9e77f3636e894372
aartaka <aartaka@protonmail.com>
2023-05-22 17:52:21 +0400

    package: Use nyxt-3 as the 3.0.0+ feature.
    Pre-releases don't matter anymore.

--- package.lisp
+++ package.lisp
@@ -22,9 +22,6 @@
 #+nyxt-3
 (nyxt:define-package #:nx-kaomoji
   (:import-from
-   #+(or 3-pre-release-1 3-pre-release-2 3-pre-release-3 3-pre-release-4 3-pre-release-5 3-pre-release-6)
-   #:nyxt/autofill-mode
-   #-(or 3-pre-release-1 3-pre-release-2 3-pre-release-3 3-pre-release-4 3-pre-release-5 3-pre-release-6)
    #:nyxt/mode/autofill
    #:autofill
    #:autofill-name
